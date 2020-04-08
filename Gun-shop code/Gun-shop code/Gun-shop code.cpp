31f828f21190adabc60de045669c2e08a54aa499
PrintHell0 <58327101+AckPrintHell0@users.noreply.github.com>
2020-04-08 11:27:13 +0300

    Update Gun-shop code.cpp
    awda

--- Gun-shop code/Gun-shop code/Gun-shop code.cpp
+++ Gun-shop code/Gun-shop code/Gun-shop code.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void startuptext() {
+    cout << "Welcome to our gun shop programme!\n";
+    cout << "What would you like to do" << endl;
+    cout << "1. Order a gun" << endl;
+    cout << "2. "
+}
+
 int main()
 {
-    cout << "Hallo \n";
+    int ans;
+    while (true) {
+        switch(ans) {
+            case 1:
+        }
+    }
 }
 
